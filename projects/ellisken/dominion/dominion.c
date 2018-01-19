d0b54bf7a2e722b301700726d45b8c4303b5b497
Kendra Ellis <ellisken@flip3.engr.oregonstate.edu>
2018-01-19 13:59:28 -0800

    Refactored cardEffect() case: adventurer in dominion.c

--- projects/ellisken/dominion/dominion.c
+++ projects/ellisken/dominion/dominion.c
@@ -660,9 +660,9 @@ int cardSmithy(int currentPlayer, struct gameState *state, int handPos)
 }
 
 		
-int cardAdventurer(int drawnTreasure, struct gameState *state, int currentPlayer, int cardDrawn)
+int cardAdventurer(int drawntreasure, struct gameState *state, int currentPlayer, int cardDrawn, int temphand[], int z)
 {
-      while(drawntreasure<2){
+    while(drawntreasure<2){
 	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
 	  shuffle(currentPlayer, state);
 	}
@@ -709,26 +709,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
-			
+        cardAdventurer(drawntreasure, state, currentPlayer, cardDrawn, temphand, z);
+
     case council_room:
       //+4 Cards
       for (i = 0; i < 4; i++)
