78090d2635f3dc5bdacb2390aa00e487f41c96cb
Kendra Ellis <ellisken@flip3.engr.oregonstate.edu>
2018-02-03 09:15:21 -0800

    Finalized unittest1.c

--- projects/ellisken/dominion/unittest1.c
+++ projects/ellisken/dominion/unittest1.c
@@ -18,6 +18,7 @@
 
 int main(){
     //Initialize gameState with 2 players
+    int i, j;
     int playerCount = 2;
     int k[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
     int seed = 3; //For randomizing the shuffling result
@@ -29,12 +30,41 @@ int main(){
 
     printf("initializeGame called\n");
 
-    assert(&testState != NULL);
 
+    //TEST 1 - check supply count returned for curse is 10
+    if(supplyCount(curse, &testState) == 10)
+        printf("---TEST 1 passed---\n");
+    else printf("---TEST 1 failed---\n");
 
-    //For each card type whose supply is necessarily filled 
-    //at initializationi (Curse, Estate, Duchy, Province, 
-    //Copper, Silver, and Gold) get the count
+    //TEST 2 - check supply count returned for victory cards is 8
+    if(supplyCount(estate, &testState) != 8)
+        printf("---TEST 2 failed---\n");
+    else if(supplyCount(duchy, &testState) != 8)
+        printf("---TEST 2 failed---\n");
+    else if(supplyCount(province, &testState) != 8)
+        printf("---TEST 2 failed---\n");
+    else printf("---TEST 2 passed---\n");
+
+    //TEST 3 - check supply count returned for treasure cards is correct
+    if(supplyCount(copper, &testState) != 60-7*playerCount)
+        printf("---TEST 3 failed---\n");
+    else if(supplyCount(silver, &testState) != 40)
+        printf("---TEST 3 failed---\n");
+    else if(supplyCount(gold, &testState) != 30)
+        printf("---TEST 3 failed---\n");
+    else printf("---TEST 3 passed---\n");
+
+    //TEST 4 - check supply count == 1 for each kingdom card in k
+    for(i=adventurer; i < treasure_map; i++){
+        for(j=0; j < 10; j++){
+            if(i == k[j]){
+                if(supplyCount(j, &testState) == 0)
+                    printf("---TEST 4 failed on kingdom card %i---\n", i);
+                else printf("---TEST 4 passed for kingdom card %i---\n", i);
+            }
+        }
+    }
+
 
     return 0;
 }
