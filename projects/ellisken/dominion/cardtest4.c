bad60a09c425665b8c015207c14d0b7c6696b483
Kendra Ellis <ellisken@flip3.engr.oregonstate.edu>
2018-02-03 21:40:01 -0800

    Finished cardtest1.c and added to makefile, added cardSmithy() declaration to dominion_helpers.h

--- projects/ellisken/dominion/cardtest4.c
+++ projects/ellisken/dominion/cardtest4.c
@@ -1,8 +1,39 @@
 /***********************************************************
- ** Program Filename: unittest1.c
+ ** Program Filename: cardtest1.c
  ** Author: Kendra Ellis, ellisken@oregonstate.edu
  ** Date: February 4, 2018
- ** Description:
- ** Input:
- ** Output:
+ ** Description: tests cardSmithy() from dominion.c
+ ** Input: function takes currentPlayer, gameState, handPos
+ ** Output: Success or Failure
  ***********************************************************/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+int main(){
+    int i, j, m;
+    int playerCount = 2;
+    int player1 = 0;
+    int seed = 3;
+    int k[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
+    struct gameState state, testState;
+
+    //Initialize game state
+    initializeGame(playerCount, k, seed, &state);
+
+    //Copy state into testState and play card
+
+    //TEST 1 - Check that exactly 3 cards were drawn from
+    //deckCount and 3 added to handCount
+
+
+
+
+
+    return 0;
+}
